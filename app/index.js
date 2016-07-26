a6945a824717e66f368a02b9cf9bd6751495882c
James Becker <jamesbecker@outlook.com>
2016-07-26 19:53:17 +0700

    added routes Home and Main

--- app/index.js
+++ app/index.js
@@ -1,54 +1,5 @@
-var USER_DATA = {
-	name: 'James Becker',
-	username: 'james-becker',
-	image: 'https://avatars1.githubusercontent.com/u/5620278?v=3&s=460'
-}
+var React = require('react');
+var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-const ProfilePic = React.createClass({
-	render: function () {
-		return (
-			<img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
-			)
-	}
-});
-
-const ProfileLink = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<a href={'https://www.github.com/' + this.props.username}>
-					{this.props.name}
-				</a>
-			</div>
-			)
-	}
-})
-
-const ProfileName = React.createClass({
-	render: function () {
-		return (
-			<div>{this.props.name}</div>
-			)
-	}
-});
-
-const Avatar = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<ProfilePic imageUrl={this.props.user.image} />
-				<ProfileName name={this.props.user.name} />
-				<ProfileLink username={this.props.user.userid} />
-			</div>
-		);
-	}
-});
-
-ReactDOM.render(
-	<Avatar user={USER_DATA} />,
-	document.getElementById('app')//keep this method in mind
-);
+ReactDOM.render(routes, document.getElementById('app'));
