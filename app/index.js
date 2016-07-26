92fa6dde3342e47e57fb8834436c8d3a0f24e0f9
James Becker <jamesbecker@outlook.com>
2016-07-26 14:03:52 +0700

    grab user profile information from github

--- app/index.js
+++ app/index.js
@@ -1,15 +1,54 @@
+var USER_DATA = {
+	name: 'James Becker',
+	username: 'james-becker',
+	image: 'https://avatars1.githubusercontent.com/u/5620278?v=3&s=460'
+}
+
+
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const HelloWorld = React.createClass({
+const ProfilePic = React.createClass({
 	render: function () {
 		return (
-			<div> Hello, React JS Program </div>
+			<img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
 			)
 	}
 });
 
+const ProfileLink = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<a href={'https://www.github.com/' + this.props.username}>
+					{this.props.name}
+				</a>
+			</div>
+			)
+	}
+})
+
+const ProfileName = React.createClass({
+	render: function () {
+		return (
+			<div>{this.props.name}</div>
+			)
+	}
+});
+
+const Avatar = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.userid} />
+			</div>
+		);
+	}
+});
+
 ReactDOM.render(
-	<HelloWorld />,
-	document.getElementById('app')
+	<Avatar user={USER_DATA} />,
+	document.getElementById('app')//keep this method in mind
 );
